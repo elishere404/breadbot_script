8ba7caaa30fc8595a0dd71fbec99363de43b507c
elis <141450459+elishere404@users.noreply.github.com>
2023-08-10 16:45:49 +0400

    Create index.js

--- index.js
+++ index.js
@@ -1,72 +1,124 @@
-const { Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.Friends],
-});
-const token = 'YOUR_BOT_TOKEN'; // Replace with your bot token
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// Store the original statuses of infected members
-const originalStatuses = new Map();
+const token = 'BOT_TOKEN';
+const targetGuildId = 'YOUR_SERVER_ID'; // Replace with your server's ID
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+let newUserCount = 0; // Initialize the count of new members
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  // Set the bot's status with an emoji
+  client.user.setActivity('DM me the word "bread" 🍞', { type: 'WATCHING' });
 });
 
 client.on('guildMemberAdd', async member => {
-    if (member.guild.id === 'YOUR_SPECIFIC_SERVER_ID') { // Replace with your server's ID
-        try {
-            await member.send(' Do NOT change your status for a week, and do NOT tell anybody what bread does, especially to a SNITCH!! 😡.');
-            console.log(`Sent infection message to ${member.user.tag}`);
+  if (member.guild.id === targetGuildId) {
+    try {
+      newUserCount++; // Increment the new user count
+      const ordinal = getOrdinal(newUserCount); // Get the ordinal suffix (e.g., 1st, 2nd, 3rd, ...)
 
-            // Store the member's original status
-            originalStatuses.set(member.id, member.presence.status);
-            
-            // Set the member's status to "DM me the word 'bread'" for a week
-            const statusMessage = 'DM me the word "bread"';
-            member.presence.setActivities([{ name: statusMessage, type: 'WATCHING' }]);
-            
-            // After a week, reset the member's status to what it was before
-            setTimeout(() => {
-                const originalStatus = originalStatuses.get(member.id) || 'online';
-                member.presence.setStatus(originalStatus);
-                originalStatuses.delete(member.id);
-            }, 7 * 24 * 60 * 60 * 1000); // Reset status after 1 week
-        } catch (error) {
-            console.error(`Failed to send infection message to ${member.user.tag}: ${error}`);
-        }
+      // Send the initial DM with the modified description
+      const dmMessage = `You have been infected.
+
+i have changed your status to “DM me the word "bread" "
+
+The infection shall spread silently. The only rule is to not spoil the game for others by telling people what saying bread does. Gotta keep the status for a week. Welcome to the new epidemic, my boy. You are the ${ordinal} member to be infected by bread. We're still trying to find the cure.`;
+      await member.send(dmMessage);
+
+      // Send the infection status embed as a DM
+      const infectionEmbed = new Discord.MessageEmbed()
+        .setTitle('Infection Status')
+        .setDescription(`We have infected our ${ordinal} member. Our infection has not yet grown.`);
+      await member.send({ embeds: [infectionEmbed] });
+
+      // Setting the infected user's status
+      await member.setPresence({
+        activities: [{ name: 'DM me the word "bread"', type: 'WATCHING' }],
+        status: 'online',
+      });
+
+      // Check if the server's member count reaches a thousand
+      const guild = await client.guilds.fetch(targetGuildId);
+      if (guild.memberCount >= 1000) {
+        await member.send('Our infection has risen. The infection has now spread to a thousand members!');
+      }
+
+      // After a week, update the infected user's status
+      setTimeout(async () => {
+        const botMember = await guild.members.fetch(client.user.id);
+
+        await member.setPresence({
+          activities: [{ name: 'DM me the word "bread"', type: 'WATCHING' }],
+          status: 'online',
+        });
+
+        // Send a message confirming the infection
+        await member.send('The infection has taken over. Keep an eye on your status for a week!');
+      }, 7 * 24 * 60 * 60 * 1000); // 1 week in milliseconds
+    } catch (error) {
+      console.error(`Failed to send message to ${member.user.tag}: ${error}`);
     }
+  }
 });
 
 client.on('message', async message => {
-    if (message.content.startsWith('/infect')) {
-        const args = message.content.split(' ');
-        const friendTag = args[1];
-        const targetUser = friendTag ? client.users.cache.find(user => user.tag === friendTag) : message.author;
-        
-        if (targetUser) {
-            try {
-                await targetUser.send('You have been infected by bread! Do NOT change your status for a week, and do NOT tell anybody what bread does, especially to a SNITCH!! 😡');
-                message.reply(`Successfully infected ${targetUser.tag} with the bread infection.`);
-            } catch (error) {
-                console.error(`Failed to infect ${targetUser.tag}: ${error}`);
-                message.reply(`Failed to infect ${targetUser.tag}.`);
-            }
-        } else {
-            const embed = new MessageEmbed()
-                .setTitle('Friend Not Found')
-                .setDescription(`The friend with the tag "${friendTag}" was not found.`);
-            message.reply({ embeds: [embed] });
-        }
-    } else if (message.content.startsWith('/about')) {
-        const embed = new MessageEmbed()
-            .setTitle('About BreadBot')
-            .setDescription('BreadBot is a fun bot that infects people with the bread! 🍞')
-            .addField('Creator', 'imyeb (elishere404 on github)')
-            .addField('Version', '1.0.0')
-            .setImage('https://media.tenor.com/jM34V0MnmkwAAAAC/fast-spinning-beget-bread.gif')
-            .setColor('#FFD700'); // Gold color
-        
-        message.reply({ embeds: [embed] });
+  if (message.content.startsWith('/infect')) {
+    const args = message.content.split(' ');
+    const friendTag = args[1];
+    const targetUser = friendTag ? client.users.cache.find(user => user.tag === friendTag) : message.author;
+
+    if (targetUser) {
+      try {
+        await targetUser.send('You have been infected by bread! Do NOT change your status for a week.');
+        message.reply(`Successfully infected ${targetUser.tag} with the bread infection.`);
+      } catch (error) {
+        console.error(`Failed to infect ${targetUser.tag}: ${error}`);
+        message.reply(`Failed to infect ${targetUser.tag}.`);
+      }
+    } else {
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Friend Not Found')
+        .setDescription(`The friend with the tag "${friendTag}" was not found.`);
+      message.reply({ embeds: [embed] });
     }
+  } else if (message.content.startsWith('/about')) {
+    const embed = new Discord.MessageEmbed()
+      .setTitle('About BreadBot')
+      .setDescription('BreadBot is a fun bot that infects people with the bread! 🍞')
+      .addField('Creator', 'imyeb (elishere404)')
+      .addField('Version', '1.0.0')
+      .setImage('https://media.tenor.com/jM34V0MnmkwAAAAC/fast-spinning-beget-bread.gif')
+      .setColor('#FFD700'); // Gold color
+
+    message.reply({ embeds: [embed] });
+  }
 });
+
+// After a week, reset the infected user's status
+client.on('presenceUpdate', async (oldPresence, newPresence) => {
+  if (
+    newPresence &&
+    newPresence.userID &&
+    newPresence.userID === newPresence.guild.me.user.id &&
+    newPresence.status === 'online'
+  ) {
+    setTimeout(async () => {
+      const botMember = await newPresence.guild.members.fetch(newPresence.userID);
+
+      await botMember.setPresence({
+        activities: [{ name: 'DM me the word "bread"', type: 'WATCHING' }],
+        status: 'online',
+      });
+    }, 7 * 24 * 60 * 60 * 1000); // Reset status after 1 week
+  }
+});
+
+function getOrdinal(number) {
+  const suffixes = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+  const remainder = number % 100;
+
+  return number + (suffixes[(remainder - 20) % 10] || suffixes[remainder] || suffixes[0]);
+}
 
 client.login(token);
